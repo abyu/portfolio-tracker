2b1327c9836c52e9baf40145ba8c41368a127885
Kishore Samudrala <abyu.kishore@gmail.com>
2026-05-07 14:52:07 +1000

    Add DB config and pool to shared state

--- src/config.rs
+++ src/config.rs
@@ -1,36 +1,43 @@
-use std::fmt;
+#[derive(Clone)]
 pub struct AppConfig {
-    pub server_port: u16
+    pub server_port: u16,
+    pub db_url: String
 }
 const DEFAULT_PORT: u16 = 3000;
 
 impl AppConfig {
     pub fn load() -> Result<AppConfig, ConfigError> {
         dotenvy::dotenv().ok();
-        let port = if let Ok(server_port) = std::env::var("PORT") {
-            if let Ok(v_port) = server_port.parse() {
-                v_port
+        let port = Self::get_env_u16_or_default("SERVER_PORT", DEFAULT_PORT)?;
+        let db_url = Self::get_env_string("DATABASE_URL")?;
+        Ok(AppConfig { server_port: port, db_url })
+    }
+
+    fn get_env_string(key: &str) -> Result<String, ConfigError> {
+        if let Ok(val) = std::env::var(key) {
+            Ok(val)
+        } else {
+            Err(ConfigError::MissingValue(key.to_string()))
+        }
+    }
+
+    fn get_env_u16_or_default(key: &str, default: u16) -> Result<u16, ConfigError> {
+        if let Ok(val) = std::env::var(key) {
+            if let Ok(v_port) = val.parse() {
+                Ok(v_port)
             } else {
-                return Err(ConfigError::InvalidPort(server_port));
+                Err(ConfigError::InvalidValue(val))
             }
         } else {
-            DEFAULT_PORT
-        };
-        Ok(AppConfig { server_port: port })
-    }
-}
-
-#[derive(Debug)]
-pub enum ConfigError {
-    InvalidPort(String)
-}
-
-impl fmt::Display for ConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ConfigError::InvalidPort(val) => write!(f, "Invalid port value: {}", val),
+            Ok(default)
         }
     }
 }
 
-impl std::error::Error for ConfigError {}
+#[derive(Debug, thiserror::Error)]
+pub enum ConfigError {
+     #[error("Invalid value: {0}")]
+    InvalidValue(String),
+    #[error("Missing config value for: {0}")]
+    MissingValue(String)
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,29 @@ use axum::{routing::get, Router};
 
 mod config;
 use config::AppConfig;
+mod app_state;
+use app_state::AppState;
+use sqlx::sqlite::SqlitePoolOptions;
+
 
 #[tokio::main]
 async fn main() {
     let config = AppConfig::load().unwrap();
 
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(config.db_url.as_str())
+        .await
+        .unwrap();
+
+    let state = AppState {
+        db: pool,
+        config: config.clone(),
+    };
+
     let app = Router::new()
-        .route("/", get(index));
+        .route("/", get(index))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind(
         format!("0.0.0.0:{}", config.server_port)
