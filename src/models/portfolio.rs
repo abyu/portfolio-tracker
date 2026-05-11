7b4b5d6cbe46a667b608508b8538d3cfdfa69f9d
Kishore Samudrala <abyu.kishore@gmail.com>
2026-05-11 14:44:39 +1000

    Add method to get portfolio summary

--- src/models/portfolio.rs
+++ src/models/portfolio.rs
@@ -7,3 +7,20 @@ pub struct Portfolio {
     pub gain_loss_cents: i64,
     pub currency: String 
 }
+
+#[derive(Debug, serde::Serialize)]
+pub struct PortfolioSummary {
+    pub total_value_price_cents: i64,
+    pub total_gain_loss_cents: i64,
+    pub currency: String
+}
+
+impl PortfolioSummary {
+    pub fn empty() -> Self {
+        Self {
+            total_value_price_cents: 0,
+            total_gain_loss_cents: 0,
+            currency: "AUD".to_string(),
+        }
+    }
+}
--- src/service/portfolio_service.rs
+++ src/service/portfolio_service.rs
@@ -1,4 +1,4 @@
-use crate::models::{portfolio::Portfolio, stock_trade::AggregatedStockTrade};
+use crate::models::{portfolio::Portfolio, portfolio::PortfolioSummary, stock_trade::AggregatedStockTrade};
 use crate::service::price_update_service::StockPriceRepository;
 use async_trait::async_trait;
 
@@ -36,6 +36,25 @@ impl<S: StockPriceRepository, P: StockTradeRepository> PortfolioService<S, P> {
             }
         }
         Ok(portfolios)
+    }
+
+    pub async fn get_summary(&self) -> Result<PortfolioSummary, PortfolioError> {
+        let portfolio = self.get_portfolio().await?;  
+        if portfolio.is_empty() {
+            return Ok(PortfolioSummary::empty());
+        }
+
+        let currency = portfolio.first().map(|f: &Portfolio| f.currency.clone()).unwrap_or("AUD".to_string());
+        let (total_value, total_gain_loss ) = portfolio.iter().fold(
+            (0i64, 0i64),
+            |acc, p| (
+                acc.0 + (p.current_price_cents as f64 * p.units).round() as i64,
+                acc.1 + p.gain_loss_cents
+            )
+        );
+
+
+        Ok(PortfolioSummary { total_value_price_cents: total_value, total_gain_loss_cents: total_gain_loss, currency: currency })
     }
 }
 
@@ -134,6 +153,57 @@ mod test{
         assert_eq!(portfolio[0].gain_loss_cents, 4000);
         assert_eq!(portfolio[0].current_price_cents, 3623);
         assert_eq!(portfolio[0].ticker, "VDHG");
+    }
+
+    #[tokio::test]
+    async fn test_empty_portfolio_summary() {
+        let prices_repo = MockRepository::new_with_prices(
+           HashMap::new()
+        );
+        let trades_repo = MockRepository::new_with_trades(vec![AggregatedStockTrade{
+            ticker: "VDHG".to_string(),
+            total_units: 20.0,
+            total_amount_cents: 68460,
+            currency: "AUD".to_string()
+        }, AggregatedStockTrade{
+            ticker: "VAS".to_string(),
+            total_units: 20.0,
+            total_amount_cents: 68460,
+            currency: "AUD".to_string()
+        }]);
+        let svc = PortfolioService::new(trades_repo, prices_repo);
+
+        let summary = svc.get_summary().await.unwrap();
+
+        assert_eq!(summary.total_gain_loss_cents, 0);
+        assert_eq!(summary.total_value_price_cents, 0);
+    }
+
+    #[tokio::test]
+    async fn test_portfolio_summary_from_all_trades() {
+        let prices_repo = MockRepository::new_with_prices(
+           [
+            ("VDHG".to_string(), StockPrice{id: 1, ticker: "VDHG".to_string(), price_cents: 6000, currency: "AUD".to_string(), fetched_at: chrono::Utc::now()}),
+            ("VAS".to_string(), StockPrice{id: 1, ticker: "VAS".to_string(), price_cents: 5000, currency: "AUD".to_string(), fetched_at: chrono::Utc::now()})
+           ].into_iter().collect()
+        );
+        let trades_repo = MockRepository::new_with_trades(vec![AggregatedStockTrade{
+            ticker: "VDHG".to_string(),
+            total_units: 20.0,
+            total_amount_cents: 80000,
+            currency: "AUD".to_string()
+        }, AggregatedStockTrade{
+            ticker: "VAS".to_string(),
+            total_units: 10.0,
+            total_amount_cents: 60000,
+            currency: "AUD".to_string()
+        }]);
+        let svc = PortfolioService::new(trades_repo, prices_repo);
+
+        let summary = svc.get_summary().await.unwrap();
+
+        assert_eq!(summary.total_gain_loss_cents, 30000);
+        assert_eq!(summary.total_value_price_cents, 170000);
     }
 
 }
